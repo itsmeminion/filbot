ec3d11db232a7c2006173349efc6ddc739707ad0
itsmeminion <88274133+itsmeminion@users.noreply.github.com>
2021-09-24 22:00:14 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,59 @@
+class script(object):
+
+
+    START_MSG = """ <b>Hi {}
+
+I'm An Autofilter Bot Made Only 4r My Owner's Channel
+
+For more click <i>help</i></b>"""
+
+
+    HELP_MSG = """<b>How to use the bot??</b>
+
+<i>
+* Add bot to your group with admin rights.
+
+* Add bot to channels which you want to link with <b>all admin rights</b>!
+</i>
+
+
+<b>Bot Commands - Works in Group only</b>
+(You need to be a Auth User in order to use these commands)
+
+* <code>/add channelid</code>  -  Links channel to your group.
+or
+* <code>/add @channelusername</code> - Links channel to your group.
+
+<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
+
+
+* <code>/del channelid</code>  -  Delinks channel from group
+or
+* <code>/del @channelusername</code>  -  Delinks channel from group
+
+<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
+
+
+* <code>/delall</code>  -  Removes all connected channels and filters from group!
+
+<i>Note : Dont add command delete bots in group! Otherwise, delall command wont work</i>
+
+
+* <code>/filterstats</code>  -  Check connected channels and number of filters.
+
+
+
+No need add each filter again!
+Bot will automatically search for your files and give links to that!
+
+
+<b>© CineBin</b>"""
+
+
+    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot </b>
+    
+⭕️<b>Creater :</b> @CineBin
+
+ 
+
+"""
